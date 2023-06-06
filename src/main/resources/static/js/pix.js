4a8a0b10c3bca78a65fc98632ee0b8940d3bca58
Thomaz Machado <thomazcm@gmail.com>
2023-06-06 18:50:01 -0300

    option for up to 4 tickets on pix link

---
+++ src/main/resources/static/js/pix.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+      $('[data-toggle="tooltip"]').tooltip();
+
+      $("#copy-button").click(function(){
+        var copyText = document.getElementById("copy-textarea");
+        navigator.clipboard.writeText(copyText.value);
+
+        $(this).tooltip('dispose');  
+        $(this).attr('title', 'Link Copiado!').tooltip();  
+        $(this).tooltip('show');
+      });
+    });
+    
+    
+
+function onLoad() {
+    var pix = new Vue({
+        el: '#pix',
+        data: {
+            pix : '',
+            display : '1 convite',
+            quantity: 1,
+            displayTotal: 'R$ 78',
+            total: 78,
+            links: [
+				'00020126480014br.gov.bcb.pix0126lauramirandalima@gmail.com520400005303986540578.005802BR5918Laura Miranda Lima6009Sao Paulo62070503***6304277B',
+				'00020126480014br.gov.bcb.pix0126lauramirandalima@gmail.com5204000053039865406156.005802BR5918Laura Miranda Lima6009Sao Paulo62070503***6304C3F7',
+				'00020126480014br.gov.bcb.pix0126lauramirandalima@gmail.com5204000053039865406234.005802BR5918Laura Miranda Lima6009Sao Paulo62070503***6304ECAD',
+				'00020126480014br.gov.bcb.pix0126lauramirandalima@gmail.com5204000053039865406312.005802BR5918Laura Miranda Lima6009Sao Paulo62070503***63048FD0'
+			]
+        },
+        mounted(){
+			
+		},
+		methods : {
+			increase() {
+				if (this.quantity >= 4) {
+					return;
+				}
+				this.quantity++;
+				this.total += 78;
+				this.display = `${this.quantity} convite`;
+				this.displayTotal = `R$ ${this.total}`;
+				if (this.quantity > 1) {
+					this.display+='s';
+				}
+			},
+			decrease() {
+				if (this.quantity <= 1) {
+					return;
+				}
+				this.quantity--;
+				this.total -= 78;
+				this.display = `${this.quantity} convite`;
+				this.displayTotal = `R$ ${this.total}`;
+				if (this.quantity > 1) {
+					this.display+='s';
+				}
+			}
+		}
+	    });
+}
