69a262e66feb7d1373383d224c907424b84d097c
Thomaz Machado <thomazcm@gmail.com>
2023-06-07 04:16:25 -0300

    allowing png requests

--- src/main/java/com/thomazcm/plantae/config/SecurityConfigurations.java
+++ src/main/java/com/thomazcm/plantae/config/SecurityConfigurations.java
@@ -29,7 +29,7 @@ public class SecurityConfigurations {
 
         http.authorizeRequests().antMatchers("/login/**", "/logout/**", "/js/**", "/css/**").permitAll()
                 .antMatchers("/link-pix/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/png/**").permitAll()
+                .antMatchers("/png/**").permitAll()
                 .anyRequest().authenticated().and().csrf().disable()
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
