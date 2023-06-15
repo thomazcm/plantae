d455d14a2294f48188f5f9dc8c0d0b007216de61
Thomaz Machado <thomazcm@gmail.com>
2023-06-15 16:27:09 -0300

    implemented JWT token auto refresh

--- src/main/java/com/thomazcm/plantae/config/AutenticacaoViaTokenFilter.java
+++ src/main/java/com/thomazcm/plantae/config/AutenticacaoViaTokenFilter.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -15,6 +16,8 @@ public class AutenticacaoViaTokenFilter extends OncePerRequestFilter{
     
     private TokenService service;
     private UsuarioRepository repository;
+    @Value("${rest.jwt.expiration}")
+    private String jwtExpiration;
     
     public AutenticacaoViaTokenFilter(TokenService service, UsuarioRepository repository) {
         this.service = service;
@@ -25,6 +28,25 @@ public class AutenticacaoViaTokenFilter extends OncePerRequestFilter{
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
         
+        String token = getToken(request);
+
+        if (token == null || !service.ehValido(token)) {
+            //fail request
+        }  else {
+            if (service.isTokenExpiring(token)) {
+                token = service.refreshToken(token);
+                Cookie jwtCookie = new Cookie("JWT-TOKEN", token);  
+                jwtCookie.setMaxAge(Integer.parseInt(jwtExpiration)* 60 * 60);  
+                jwtCookie.setHttpOnly(true);
+                jwtCookie.setPath("/");
+                response.addCookie(jwtCookie);
+            } 
+            autenticaRequest(token);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
         String token = null;
 
@@ -36,18 +58,7 @@ public class AutenticacaoViaTokenFilter extends OncePerRequestFilter{
                 }
             }
         }
-
-        if (token != null && service.ehValido(token)) {
-            autenticaRequest(token);
-        }
-        
-//        String token = request.getHeader("Authorization");
-//        boolean ehValido = service.ehValido(token);
-//        
-//        if (ehValido) {
-//            autenticaRequest(token);
-//        }
-        filterChain.doFilter(request, response);
+        return token;
     }
 
     private void autenticaRequest(String token) {
--- src/main/java/com/thomazcm/plantae/config/TokenService.java
+++ src/main/java/com/thomazcm/plantae/config/TokenService.java
@@ -2,6 +2,8 @@ package com.thomazcm.plantae.config;
 
 import java.util.Date;
 import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,9 @@ import io.jsonwebtoken.UnsupportedJwtException;
 
 @Service
 public class TokenService {
+    
+    @Autowired
+    private UsuarioRepository repository;
     
     @Value("${rest.jwt.expiration}")
     private String expiration;
@@ -37,6 +42,37 @@ public class TokenService {
                 .setExpiration(dataExpiracao)
                 .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
+    }
+    
+
+    public String refreshToken(String token) {
+        Usuario logado = usuarioFromToken(token, repository);
+        Date hoje = new Date();
+        Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration)* 60 * 60 * 1000);
+        return Jwts.builder()
+                .setIssuer("plantae")
+                .setSubject(logado.getId().toString())
+                .setIssuedAt(hoje)
+                .setExpiration(dataExpiracao)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    
+    public boolean isTokenExpiring(String token) {
+        Date expirationDate = getExpirationDateFromToken(token);
+        Date now = new Date();
+        long differenceInMilliseconds = expirationDate.getTime() - now.getTime();
+        long hoursLeft = TimeUnit.MILLISECONDS.toHours(differenceInMilliseconds);
+        return hoursLeft < 24;
+    }
+    
+    private Date getExpirationDateFromToken(String token) {
+        Claims claims = Jwts.parser()
+            .setSigningKey(secret)
+            .parseClaimsJws(token)
+            .getBody();
+        return claims.getExpiration();
     }
 
     public boolean ehValido(String tokenRaw) {
@@ -70,5 +106,7 @@ public class TokenService {
         }
         return tokenRaw;
     }
+
+    
 
 }
