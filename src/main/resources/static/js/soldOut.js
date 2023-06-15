623abbed7d700c7c63ee9cd671a029c41fbad2ab
Thomaz Machado <thomazcm@gmail.com>
2023-06-14 21:50:20 -0300

    updated fonts

---
+++ src/main/resources/static/js/soldOut.js
@@ -0,0 +1,2 @@
+ document.getElementById('soldOutText1').innerText = config.textConfigurations.soldOutText1;
+ document.getElementById('soldOutText2').innerText = config.textConfigurations.soldOutText2;
