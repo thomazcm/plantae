9ef5e957ddc651d9c7ab8aaf4a4bb7fddd0d24d9
Thomaz Machado <thomazcm@gmail.com>
2023-06-30 12:23:57 -0300

    added reminder email test

--- src/main/java/com/thomazcm/plantae/api/EmailApi.java
+++ src/main/java/com/thomazcm/plantae/api/EmailApi.java
@@ -1,11 +1,13 @@
 package com.thomazcm.plantae.api;
 
 import java.util.List;
+import org.apache.regexp.RE;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.thomazcm.plantae.api.service.EmailApiService;
+import com.thomazcm.plantae.api.service.PlantaeEmailSender;
 import com.thomazcm.plantae.config.properties.MailProperties;
 import com.thomazcm.plantae.model.Ingresso;
 import com.thomazcm.plantae.repository.IngressoRepository;
@@ -17,15 +19,17 @@ public class EmailApi {
     private final IngressoRepository ingressoRepo;
     private final MailProperties mailProperties;
     private final EmailApiService emailApiService;
+    private final PlantaeEmailSender sender;
     
     private static final String SURVEY_EMAIL_TEMPLATE = "surveyEmailTemplate";
     private static final String REMINDER_EMAIL_TEMPLATE = "reminderEmailTemplate";
     
     public EmailApi(IngressoRepository ingressoRepo, MailProperties mailProperties,
-            EmailApiService emailApiService) {
+            EmailApiService emailApiService, PlantaeEmailSender sender) {
         this.ingressoRepo = ingressoRepo;
         this.mailProperties = mailProperties;
         this.emailApiService = emailApiService;
+        this.sender = sender;
     }
 
     @GetMapping("/list")
@@ -44,6 +48,13 @@ public class EmailApi {
         String subject = mailProperties.getReminderSubject();
         emailApiService.sendEmails(REMINDER_EMAIL_TEMPLATE, subject, getAllDistinctCustomerEmails());
         return ResponseEntity.ok().build();
+    }
+    
+    @GetMapping("/send-reminder-test")
+    public ResponseEntity<String> sendReminderTestMail() {
+        String subject = mailProperties.getReminderSubject();
+        sender.sendEmailTemplate("thomazcm@gmail.com", subject, REMINDER_EMAIL_TEMPLATE);
+        return ResponseEntity.ok("sent");
     }
     
     @GetMapping("/list-bundles")
